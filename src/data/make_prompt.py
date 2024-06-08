38fe64896554566d4edb1b90b25af2a7c0571d29
dendroenydris <yuxin.xue@campus.tu-berlin.de>
2024-06-09 01:24:33 +0200

    change variable names

--- src/data/make_prompt.py
+++ src/data/make_prompt.py
@@ -1,19 +1,18 @@
-def formatting_prompts_func(prompt, example):
+def formatting_prompts_func(prompt_template, example):
     """Create a list to store the formatted texts for each item in the example
 
     Args:
-        example (list of dataset): one batch from dataset. each line might consist of prompt context and question
+        example (list of dataset): one batch from dataset. each line might consist of prompt_template context and question
     Returns:
-        formatted_texts: formated prompts
+        prompt_texts: formated prompt_templates
     """
 
-    formatted_texts = []
-
+    prompt_texts = []
     # Iterate through each example in the batch
-    for context, question in zip(example['context'], example['question']):
-        # Format each example as a prompt-response pair
-        formatted_text = prompt.replace('[question]', question)
-        formatted_text = prompt.replace('[context]', "Context: "+context)
-        formatted_texts.append(formatted_text)
+    for context, question, raw_label in zip(example['context'], example['question'], example['relation']):
+        # Format each example as a prompt_template-response pair
+        prompt_text = prompt_template.format(
+            question=question, context=context)
+        prompt_texts.append(prompt_text+raw_label)
     # Return the list of formatted texts
-    return formatted_texts
+    return prompt_texts
