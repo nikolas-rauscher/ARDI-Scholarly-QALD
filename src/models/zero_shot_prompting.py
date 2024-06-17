1a8957c59ebab4c1ba0b0281b14abb681573925b
dendroenydris <yuxin.xue@campus.tu-berlin.de>
2024-06-17 14:37:22 +0200

    init

--- src/models/zero_shot_prompting.py
+++ src/models/zero_shot_prompting.py
@@ -1,5 +1,48 @@
-def load_model(root_path):
-    pass
+import features
+from data.make_prompt import formatting_prompts_func
+from features.evidence_selection import evidence_triple_selection
+import pandas as pd
+from transformers import pipeline
+import torch
+import json
+import sys
+sys.path.append('./src')
 
-def zero_shot_prompting(data,evidence_selection):
-    pass
+
+def load_pipeline(model_path="/Volumes/T7/Backup/models/meta-llama/Meta-Llama-3-8B"):
+    return pipeline(
+        "text-generation", model=model_path, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto"
+    )
+
+def zero_shot_prompting(pipeline, examples, evidence_selection=False):
+    """
+    Generate answers to questions using a zero-shot learning approach with the provided pipeline.
+
+    Args:
+        pipeline (transformers.Pipeline): The text-generation pipeline initialized with a specific model.
+        examples (list of dict): A batch of examples, where each example includes a 'context' (list of triples) and a 'question' (str).
+        evidence_selection (bool, optional): Flag to determine if evidence selection should be performed. Defaults to False.
+    Returns:
+        list of str: The generated responses for each input example.
+    """
+    if (evidence_selection):
+        for example in examples:
+            example['context'] = evidence_triple_selection(
+                example['question'], example['all_tripples'])
+    else:
+        example['context'] = example['all_tripples']
+    formatted_prompts = formatting_prompts_func(prompt_template, examples)
+    responses = []
+    for formatted_prompt in formatted_prompts:
+        response = pipeline(formatted_prompt)
+        responses.append(response[0]['generated_text'])
+    return responses
+
+
+if __name__ == '__main__':
+    llama3 = load_pipeline()
+    with open("../../data/raw/prompt_template.txt", "r") as file:
+        prompt_template = file.read()
+    with open('../../data/processed/test_processed_data.json', 'r') as file:
+        examples = json.load(file)
+    responses = zero_shot_prompting(llama3, prompt_template)
