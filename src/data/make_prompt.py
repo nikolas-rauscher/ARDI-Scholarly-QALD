9b9b5dae99fbee64f2d42f6a700eb57db48b2d23
dendroenydris <yuxin.xue@campus.tu-berlin.de>
2024-06-17 15:44:35 +0200

    fix bugs add test for initial version

--- src/data/make_prompt.py
+++ src/data/make_prompt.py
@@ -9,10 +9,10 @@ def formatting_prompts_func(prompt_template, example):
 
     prompt_texts = []
     # Iterate through each example in the batch
-    for context, question, raw_label in zip(example['context'], example['question'], example['relation']):
+    for context, question in zip(example['context'], example['question']):
         # Format each example as a prompt_template-response pair
         prompt_text = prompt_template.format(
             question=question, context=context)
-        prompt_texts.append(prompt_text+raw_label)
+        prompt_texts.append(prompt_text)
     # Return the list of formatted texts
     return prompt_texts
--- src/models/zero_shot_prompting.py
+++ src/models/zero_shot_prompting.py
@@ -1,5 +1,4 @@
 import features
-from data.make_prompt import formatting_prompts_func
 from features.evidence_selection import evidence_triple_selection, triple2text
 import pandas as pd
 from transformers import pipeline
@@ -15,7 +14,27 @@ def load_pipeline(model_path="/Volumes/T7/Backup/models/meta-llama/Meta-Llama-3-
     )
 
 
-def zero_shot_prompting(pipeline, examples, evidence_selection=False):
+def formatting_prompts_func(prompt_template, examples, max_length=4096):
+    """Create a list to store the formatted texts for each item in the example
+
+    Args:
+        example (list of dataset): one batch from dataset. each line might consist of prompt_template context and question
+    Returns:
+        prompt_texts: formated prompt_templates
+    """
+
+    prompt_texts = []
+    # Iterate through each example in the batch
+    for example in examples:
+        # Format each example as a prompt_template-response pair
+        prompt_text = prompt_template.format(
+            question=example['question'], context=example['context'][:max_length-len(example['question'])-len(prompt_template)])
+        prompt_texts.append(prompt_text)
+    # Return the list of formatted texts
+    return prompt_texts
+
+
+def zero_shot_prompting(pipeline, examples, prompt_template="", evidence_selection=False, verbalizer=False, max_length=4096):
     """
     Generate answers to questions using a zero-shot learning approach with the provided pipeline.
 
@@ -25,15 +44,19 @@ def zero_shot_prompting(pipeline, examples, evidence_selection=False):
         evidence_selection (bool, optional): Flag to determine if evidence selection should be performed. Defaults to False.
     Returns:
         list of str: The generated responses for each input example.
-    TODO: add verbaliser
+    TODO: add verbalizer
     """
     for example in examples:
         triples = evidence_triple_selection(
             example['question'], example['all_tripples']) if evidence_selection else example['all_tripples']
-        example['context'] = ''.join(
-            [triple2text(triple) for triple in triples])
+        if (not verbalizer):
+            example['context'] = ''.join(
+                [triple2text(triple) for triple in triples])
+        else:
+            example['context'] = ""
 
-    formatted_prompts = formatting_prompts_func(prompt_template, examples)
+    formatted_prompts = formatting_prompts_func(
+        prompt_template, examples, max_length)
     responses = []
     for formatted_prompt in formatted_prompts:
         response = pipeline(formatted_prompt)
