909c5dd27bc292a75532c3a2afe80fbea047b1e4
Nikolas Rauscher <nikolas.rauscher@gmail.com>
2024-06-19 19:14:09 +0200

    testing
    Signed-off-by: Nikolas Rauscher <nikolas.rauscher@gmail.com>

--- src/models/verbalizer/generatePrompt.py
+++ src/models/verbalizer/generatePrompt.py
@@ -6,8 +6,12 @@ from verbalisation_module import VerbModule
 def group_triples(tripleList):
     predicateDict = defaultdict(lambda: defaultdict(set))
     for item in tripleList:
-        predicateDict[item['predicate']]['subjects'].add(item['subject'])
-        predicateDict[item['predicate']]['objects'].add(item['object'])
+        subjects = item['subject'] if isinstance(item['subject'], list) else [item['subject']]
+        objects = item['object'] if isinstance(item['object'], list) else [item['object']]
+        for subj in subjects:
+            predicateDict[item['predicate']]['subjects'].add(subj)
+        for obj in objects:
+            predicateDict[item['predicate']]['objects'].add(obj)
     return predicateDict
 
 def format_triples(predicateDict):
@@ -19,9 +23,9 @@ def format_triples(predicateDict):
         objects = list(entities['objects'])
 
         if len(subjects) == 1 and len(objects) > 5:
-            large_lists.append({"subject": subjects[0], "predicate": predicate, "object": list(objects)})
+            large_lists.append({"subject": subjects[0], "predicate": predicate, "object": objects})
         elif len(objects) == 1 and len(subjects) > 5:
-            large_lists.append({"subject": list(subjects), "predicate": predicate, "object": objects[0]})
+            large_lists.append({"subject": subjects, "predicate": predicate, "object": objects[0]})
         else:
             for subj in subjects:
                 for obj in objects:
@@ -33,50 +37,49 @@ def format_triples(predicateDict):
     formatted_triples.extend(large_lists)  # Add the large lists at the end
     return formatted_triples
 
-def print_triples(triples, title):
-    print(title)
-    for triple in triples:
-        print(triple)
-        print("\n")
-
 def preprocess_triples(tripleList):
-    # Sort triples by predicate
     tripleList.sort(key=lambda x: x['predicate'])
-    
     predicateDict = group_triples(tripleList)
     formatted_triples = format_triples(predicateDict)
-    
-    # Print the sorted form
-    print_triples(tripleList, "Sorted Triples:")
-    
-    # Print the grouped and formatted triples
-    print_triples(formatted_triples, "Grouped Triples:")
-    
     return formatted_triples
 
 def verbalise(tripleList, verbModule):
-    ans = "translate Graph to English: "
-    for item in tripleList:
-        if isinstance(item['object'], list):
-            ans += f"<H> {item['subject']} <R> {item['predicate']} <T> {', '.join(item['object'])} "
-        elif isinstance(item['subject'], list):
-            ans += f"<H> {', '.join(item['subject'])} <R> {item['predicate']} <T> {item['object']} "
+    final_ans_list = []
+    predicateDict = group_triples(tripleList)
+    formatted_triples = format_triples(predicateDict)
+
+    for predicate, entities in predicateDict.items():
+        ans = "translate Graph to English: "
+        subjects = list(entities['subjects'])
+        objects = list(entities['objects'])
+        
+        if len(subjects) == 1 and len(objects) > 5:
+            ans += f'<H> "{subjects[0]}" <R> "{predicate}" <T> "{", ".join(objects)}"'
+        elif len(objects) == 1 and len(subjects) > 5:
+            ans += f'<H> "{", ".join(subjects)}" <R> "{predicate}" <T> "{objects[0]}"'
         else:
-            ans += f"<H> {item['subject']} <R> {item['predicate']} <T> {item['object']} "
-        ans += "\n"
-    return verbModule.verbalise(ans)
+            for subj in subjects:
+                for obj in objects:
+                    ans += f'<H> "{subj}" <R> "{predicate}" <T> "{obj}"'
+        
+        final_ans_list.append(verbModule.verbalise(ans))
+
+    return final_ans_list
 
 def plainPrompt(tripleList):
     ans = ""
-    for item in tripleList:
+    predicateDict = group_triples(tripleList)
+    formatted_triples = format_triples(predicateDict)
+
+    for item in formatted_triples:
         if isinstance(item['object'], list):
-            ans += f"{item['subject']} {item['predicate']} {', '.join(item['object'])}. "
+            ans += f'"{item["subject"]}" "{item["predicate"]}" "{", ".join(item["object"])}".'
         elif isinstance(item['subject'], list):
-            ans += f"{', '.join(item['subject'])} {item['predicate']} {item['object']}. "
+            ans += f'"{", ".join(item["subject"])}" "{item["predicate"]}" "{item["object"]}".'
         else:
-            ans += f"{item['subject']} {item['predicate']} {item['object']}. "
+            ans += f'"{item["subject"]}" "{item["predicate"]}" "{item["object"]}".'
         ans += "\n"
-    return ans
+    return ans.strip()
 
 def verbaliseFile(FILENAME, outputFile, limit):
     results = []
@@ -93,12 +96,19 @@ def verbaliseFile(FILENAME, outputFile, limit):
         preprocessed_triples = preprocess_triples(item['all_tripples'])
 
         oneItem['plain_prompt'] = plainPrompt(preprocessed_triples) 
-        oneItem['verbalised_prompt'] = verbalise(preprocessed_triples, verb_module)
+        verbalised_list = verbalise(preprocessed_triples, verb_module)
+        oneItem['verbalised_prompt'] = "\n".join(verbalised_list)
+
+        oneItem['triples'] = {
+            "preprocessed": preprocessed_triples,
+            "verbalised": verbalised_list
+        }
         results.append(oneItem)
+        
     with open(outputFile, "w", encoding='utf-8') as outfile:
         json.dump(results, outfile, indent=4)
 
 if __name__ == "__main__":
     FILENAME = "processed_data.json"
     outputFile = "verbalised_data.json"
-    verbaliseFile(FILENAME, outputFile, limit=3) # add limit for testing
+    verbaliseFile(FILENAME, outputFile, limit=1)
