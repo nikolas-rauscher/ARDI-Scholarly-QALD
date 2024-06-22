a6b6691cf3a15fd3f3f67136fd9f06d6302573b1
Nikolas Rauscher <nikolas.rauscher@gmail.com>
2024-06-22 17:22:29 +0200

    style(zero shot prompting): better naming for the pipeline #32 #28 #27 #23
    Signed-off-by: Nikolas Rauscher <nikolas.rauscher@gmail.com>

--- src/models/main.py
+++ src/models/main.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append('./src')
 def main():
     os.system("python3 src/models/prepare_prompts_context.py")
-    os.system("python3 src/models/zero_shot_prompting2.py")
+    os.system("python3 src/models/zero_shot_prompting_pipeline.py")
 
 if __name__ == '__main__':
     main()
