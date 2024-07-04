fe50f09a825849593f0bee793c4b09f6acb34e0b
Erik Rubinov <erik.rubinov@ottobock.com>
2024-07-04 17:09:43 +0200

    update script

--- src/data/create_dataset_alex.py
+++ src/data/create_dataset_alex.py
@@ -6,115 +6,121 @@ import json
 from run_query import run_query
 
 
-def get_data() -> dict : 
-    outputdata_name_path_0 = "data/processed/processed_data3_0.json"
-    outputdata_name_path_1 = "data/processed/processed_data3_1.json"
+def read_json_(outputdata_name_path) -> dict:
+    with open(outputdata_name_path, 'r', encoding='utf-8') as file:
+        formulations = json.load(file)
+    return formulations
 
-    def read_json_(outputdata_name_path) -> dict:
-        with open(outputdata_name_path, 'r', encoding='utf-8') as file:
-            formulations = json.load(file)
-        return formulations
+def create_alex_dataset(dblp_processed_dataset_path, outputdata_path):
+    data = read_json_(dblp_processed_dataset_path) 
 
-    data_0 = read_json_(outputdata_name_path_0)
-    data_1 = read_json_(outputdata_name_path_1)
+    alex_all_questions = []
 
-    data = data_0 + data_1 
-    return data
+    for question, iteration in zip(data, range(len(data))):
+        print(iteration)
+        alex_question = {}
+        alex_question["id"] = question["id"]
+        alex_question["question"] = question["question"]
+        alex_question["answer"] = question["answer"]
+        alex_question["author_uri"] = []
+        alex_question["all_tripples"] = []
 
-alex_all_questions = []
+        orcid = ""
+        for dblp_entity in question["all_tripples"]: #go over every entity in quesiton 
+            entity_dict = {}
+            for tripple in dblp_entity["tripples"]:
+                if tripple["predicateLabel"] == "orcid":
+                    orcid = tripple["object"]
+                    entity_dict["entity"] = orcid
+                
 
-data = get_data() 
-data = data[:3000]
-for question, iteration in zip(data, range(len(data))):
-    print(iteration)
-    alex_question = {}
-    alex_question["id"] = question["id"]
-    alex_question["question"] = question["question"]
-    alex_question["answer"] = question["answer"]
 
-    orcid = ""
-    for tripple in question["all_tripples"]:
-        if tripple["predicateLabel"] == "orcid":
-            orcid = tripple["object"]
+            query = f"""
+                PREFIX dbo: <https://dbpedia.org/ontology/>
+                PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 
-    query = f"""
-        PREFIX dbo: <https://dbpedia.org/ontology/>
-        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-
-        SELECT ?author ?name
-            WHERE {{
+                SELECT ?author ?name
+                    WHERE {{
+                    
+                    ?author foaf:name ?name .
+                    ?author dbo:orcidId "{orcid}" .
             
-            ?author foaf:name ?name .
-            ?author dbo:orcidId "{orcid}" .
-    
-        }}
-    """
-    query_res = run_query(query)  
-    if query_res['results']['bindings']:
+                }}
+            """
+            query_res = run_query(query)  
+            if query_res['results']['bindings']:
 
-        author_url_alex = query_res['results']['bindings'][0]['author']['value']
-        alex_question["author_uri"] = author_url_alex
+                author_url_alex = query_res['results']['bindings'][0]['author']['value']
+                #alex_question["author_uri"] =  author_url_alex
+                alex_question["author_uri"].append(author_url_alex)
 
-        author_name = query_res['results']['bindings'][0]['name']['value']
+                author_name = query_res['results']['bindings'][0]['name']['value']
 
-    #{'head': {'vars': ['author', 'name']}, 'results': {'bindings': [{'author': {'type': 'uri', 'value': 'https://semopenalex.org/author/A5069855349'}, 'name': {'type': 'literal', 'value': 'Tom Wilson'}}]}}
-    query = f"""
+            #{'head': {'vars': ['author', 'name']}, 'results': {'bindings': [{'author': {'type': 'uri', 'value': 'https://semopenalex.org/author/A5069855349'}, 'name': {'type': 'literal', 'value': 'Tom Wilson'}}]}}
+            query = f"""
 
-        SELECT ?predicate ?object
-        WHERE {{
-            <{author_url_alex}>  ?predicate ?object.
-        }}
+                SELECT ?predicate ?object
+                WHERE {{
+                    <{author_url_alex}>  ?predicate ?object.
+                }}
 
-        """
-    query_res = run_query(query)
+                """
+            query_res = run_query(query)
 
-    tripples = []
-    # Iterate over each binding in the data
-    for binding in query_res['results']['bindings']:
-        # Each entry in the list is a dictionary with 'Subject', 'Predicate', 'Object'
-        entry = {
-            "subject": author_name,
-            "predicate": (binding['predicate']['value']).split('/')[-1] ,
-            "object": binding['object']['value']
-        }
-        tripples.append(entry)
+            tripples = []
+            # Iterate over each binding in the data
+            for binding in query_res['results']['bindings']:
+                # Each entry in the list is a dictionary with 'Subject', 'Predicate', 'Object'
+                entry = {
+                    "subject": author_name,
+                    "predicate": (binding['predicate']['value']).split('/')[-1] ,
+                    "object": binding['object']['value']
+                }
+                tripples.append(entry)
 
-    query = f"""
+            query = f"""
 
-        SELECT ?subject ?predicate ?object 
-        WHERE {{
-            ?subject  ?predicate <{author_url_alex}>.
-        }}
+                SELECT ?subject ?predicate ?object 
+                WHERE {{
+                    ?subject  ?predicate <{author_url_alex}>.
+                }}
 
-        """
-    query_res = run_query(query)
+                """
+            query_res = run_query(query)
 
-    # Iterate over each binding in the data
-    for binding in query_res['results']['bindings']:
-        # Each entry in the list is a dictionary with 'Subject', 'Predicate', 'Object'
-        entry = {
-            "subject": binding['subject']['value'],
-            "predicate": (binding['predicate']['value']).split('/')[-1], 
-            "object": author_name
-        }
-        tripples.append(entry)
+            # Iterate over each binding in the data
+            for binding in query_res['results']['bindings']:
+                # Each entry in the list is a dictionary with 'Subject', 'Predicate', 'Object'
+                entry = {
+                    "subject": binding['subject']['value'],
+                    "predicate": (binding['predicate']['value']).split('/')[-1], 
+                    "object": author_name
+                }
+                tripples.append(entry)
 
-    alex_question["tripples_number"] = len(tripples)
 
-    alex_question["all_tripples"] = tripples
+            entity_dict["tripples"] = tripples
 
-    alex_all_questions.append(alex_question)
+            alex_question["all_tripples"].append(entity_dict)
 
-file_path = "data/processed/processed_data_alex_01.json"
-with open(file_path, 'w') as file:
-    json.dump(alex_all_questions, file, indent=4,ensure_ascii=False)
+            for entity in alex_question["all_tripples"]:
+                tripples_number = 0
+                tripples_number += len(entity["tripples"])
+            alex_question["tripples_number"] = tripples_number
+            
+        alex_all_questions.append(alex_question)
+
+    with open(outputdata_path, 'w') as file:
+        json.dump(alex_all_questions, file, indent=4,ensure_ascii=False)
 
 
 
 ##############################################################################
 def main():
 
-   get_data()
+    dblp_processed_dataset_path = "data/processed/dblp/pre_processed_data1000.json"
+    outputdata_path = "data/processed/alex/pre_processed_data1000.json"
+    create_alex_dataset(dblp_processed_dataset_path, outputdata_path)
 ##############################################################################
 if __name__ == "__main__":
     main()
