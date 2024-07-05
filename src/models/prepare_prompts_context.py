96efc9aa971fd8fb28fa240371d35e262a5f0793
Nikolas Rauscher <nikolas.rauscher@gmail.com>
2024-07-05 14:33:57 +0200

    remove experience
    Signed-off-by: Nikolas Rauscher <nikolas.rauscher@gmail.com>

--- src/models/prepare_prompts_context.py
+++ src/models/prepare_prompts_context.py
@@ -1,5 +1,5 @@
-import json
 import configparser
+import json
 from tqdm import tqdm
 import sys
 import os
@@ -13,14 +13,25 @@ config.read('config.ini')
 
 
 def prepare_data(examples, prompt_template, output_file):
+    """
+    Prepare the data by generating contexts for each example.
+
+    Args:
+        examples (list): List of examples.
+        prompt_template (str): Prompt template.
+        output_file (str): Output file path.
+
+    Returns:
+        None
+    """
     prepared_data = []
     for example in tqdm(examples, desc="Preparing Data"):
-        # Anzahl der Tripel
+  
         tripples_number = len(example['all_tripples'])
 
         # Plain Triples
-        context_plain = '. '.join([triple2text(triple)
-                                  for triple in example['all_tripples']])
+        # context_plain = '. '.join([triple2text(triple)
+        #                           for triple in example['all_tripples']])
 
         # Evidence Matching
         triples_evidence = evidence_triple_selection(
@@ -29,7 +40,7 @@ def prepare_data(examples, prompt_template, output_file):
             [triple2text(triple) for triple in triples_evidence])
 
         # Verbalizer
-        context_verbalizer = verbalise_triples(example['all_tripples'])
+        # context_verbalizer = verbalise_triples(example['all_tripples'])
 
         # Verbalizer + Evidence Matching
         context_evidence_verbalizer = verbalise_triples(triples_evidence)
@@ -40,9 +51,9 @@ def prepare_data(examples, prompt_template, output_file):
             # "answer": example["answer"],
             "tripples_number": tripples_number,
             "contexts": {
-                "all_tripples": example['all_tripples'],
-                "plain": context_plain,
-                "verbalizer_on_all_tripples": context_verbalizer,
+                # "all_tripples": example['all_tripples'],
+                # "plain": context_plain,
+                # "verbalizer_on_all_tripples": context_verbalizer,
                 "evidence_matching": context_evidence,
                 "verbalizer_plus_evidence_matching": context_evidence_verbalizer
             }
@@ -57,6 +68,17 @@ def prepare_data(examples, prompt_template, output_file):
 
 
 def process_file(input_file_path, prompt_template_path, output_file_path):
+    """
+    Process the input file and generate prepared data.
+
+    Args:
+        input_file_path (str): Input file path.
+        prompt_template_path (str): Prompt template file path.
+        output_file_path (str): Output file path.
+
+    Returns:
+        bool: True if the process is successful, False otherwise.
+    """
     if not os.path.exists(input_file_path):
         print(f"Error: Input file '{input_file_path}' not found.")
         return False
@@ -73,27 +95,10 @@ def process_file(input_file_path, prompt_template_path, output_file_path):
     return True
 
 if __name__ == '__main__':
-    input_files = [
-        'data/external/train_post_processed_data_dblp_hm.json',
-        'data/external/train_post_processed_data_alex_hm.json',
-        'data/external/train_hm_openalex_dblp.json'
-    ]
+    with open(config['FilePaths']['test_data_file'], 'r') as file:
+        examples = json.load(file)
+    with open(config['FilePaths']['prompt_template'], 'r') as file:
+        prompt_template = file.read()
     
-    output_files = [
-        './results/prepared_data_hm_openalex_dblp.json',
-        './results/prepared_data_alex_hm.json',
-        './results/prepared_data_dblp_hm.json'
-    ]
-    
-    prompt_template_path = config['FilePaths']['prompt_template']
-
-    all_files_exist = all(os.path.exists(f) for f in input_files + [prompt_template_path])
-    if not all_files_exist:
-        print("Error: One or more input files or the prompt template file are missing.")
-        sys.exit(1)
-
-    for input_file, output_file in zip(input_files, output_files):
-        success = process_file(input_file, prompt_template_path, output_file)
-        if not success:
-            print(f"Processing stopped due to missing file: {input_file}")
-            break
+    output_file = config['FilePaths']['prepared_data_file']
+    prepare_data(examples, prompt_template, output_file)
