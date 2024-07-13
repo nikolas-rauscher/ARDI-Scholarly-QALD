0cae08cc452fe1b757144a47c6a8af354cfdbc0a
dendroenydris <yuxin.xue@campus.tu-berlin.de>
2024-07-13 20:08:16 +0200

    fix bugs #52 and wiki context error

--- src/models/prepare_prompts_context.py
+++ src/models/prepare_prompts_context.py
@@ -1,22 +1,25 @@
+sys.path.append('./src')
+sys.path.append('..')
+from features.evidence_selection import evidence_triple_selection, triple2text, evidence_sentence_selection
+from models.verbalizer.generatePrompt import verbalise_triples
 import json
 import configparser
 from tqdm import tqdm
 import sys
 import os
 PACKAGE_PARENT = '.'
-SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+SCRIPT_DIR = os.path.dirname(os.path.realpath(
+    os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
-PREFIX_PATH = "/".join(os.path.dirname(os.path.abspath(__file__)).split("/")[:-2]) + "/"
+PREFIX_PATH = "/".join(os.path.dirname(os.path.abspath(__file__)
+                                       ).split("/")[:-2]) + "/"
 print(PREFIX_PATH)
-sys.path.append('..')
-config = configparser.ConfigParser()
-config.read(PREFIX_PATH +'config.ini')
-sys.path.append('./src')
-from features.evidence_selection import evidence_triple_selection, triple2text,evidence_sentence_selection
-from models.verbalizer.generatePrompt import verbalise_triples
 
 config = configparser.ConfigParser()
+config.read(PREFIX_PATH + 'config.ini')
+config = configparser.ConfigParser()
 config.read('config.ini')
+
 
 def prepare_data_only_ve(examples, prompt_template, output_file):
     """
@@ -31,7 +34,7 @@ def prepare_data_only_ve(examples, prompt_template, output_file):
         None
     """
     prepared_data = []
-    
+
     for example in tqdm(examples, desc="Preparing Data"):
         # Number of triples
         triples_number = len(example['all_triples'])
@@ -43,33 +46,33 @@ def prepare_data_only_ve(examples, prompt_template, output_file):
 
         # Verbalizer + Evidence Matching
         context_evidence_verbalizer = verbalise_triples(triples_evidence)
-        
+
         wiki_context = ""
         for wiki_text in example['wiki_data']:
             # sentences = ['. '.join(list(item.values())) for item in wiki_text]
-            sentences=str(wiki_text).split('.')
-            wiki_evidence = evidence_sentence_selection(
-                example['question'], sentences, conserved_percentage=0.2, max_num=40
-            )
-            wiki_context = '. '.join(wiki_evidence)
+            if len(wiki_text) > 0:
+                sentences = str(wiki_text).split('.')
+                wiki_evidence = evidence_sentence_selection(
+                    example['question'], sentences, conserved_percentage=0.2, max_num=40
+                )
+                wiki_context += '. '.join(wiki_evidence)
         prepared_example = {
             "id": example["id"],
             "question": example["question"],
             "triples_number": triples_number,
             "contexts": context_evidence_verbalizer + wiki_context
         }
-        
+
         if "answer" in example:
             prepared_example["answer"] = example["answer"]
-            
+
         prepared_data.append(prepared_example)
 
     with open(output_file, 'w') as file:
         json.dump(prepared_data, file, indent=4, ensure_ascii=False)
 
 
-
-def prepare_data(examples, prompt_template, output_file):
+def prepare_data_openAlex_dblp(examples, prompt_template, output_file):
     """
     Prepare the data by generating contexts for each example with dnlp and openalex
 
@@ -83,7 +86,7 @@ def prepare_data(examples, prompt_template, output_file):
     """
     prepared_data = []
     for example in tqdm(examples, desc="Preparing Data"):
-  
+
         tripples_number = len(example['all_triples'])
 
         # Plain Triples
@@ -140,22 +143,24 @@ def process_file(input_file_path, prompt_template_path, output_file_path):
         print(f"Error: Input file '{input_file_path}' not found.")
         return False
     if not os.path.exists(prompt_template_path):
-        print(f"Error: Prompt template file '{prompt_template_path}' not found.")
+        print(
+            f"Error: Prompt template file '{prompt_template_path}' not found.")
         return False
-    
+
     with open(input_file_path, 'r') as file:
         examples = json.load(file)
     with open(prompt_template_path, 'r') as file:
         prompt_template = file.read()
-    
+
     prepare_data_only_ve(examples, prompt_template, output_file_path)
     return True
+
 
 if __name__ == '__main__':
     with open(config['FilePaths']['prepare_prompt_context_input'], 'r') as file:
         examples = json.load(file)
     with open(config['FilePaths']['prompt_template'], 'r') as file:
         prompt_template = file.read()
-    
+
     output_file = config['FilePaths']['prepared_data_file']
     prepare_data_only_ve(examples, prompt_template, output_file)
