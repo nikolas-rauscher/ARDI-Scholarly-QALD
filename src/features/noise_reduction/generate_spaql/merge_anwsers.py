ff435024e28bb2b8a0cb403405b61b017fbcf03c
Nikolas Rauscher <nikolas.rauscher@gmail.com>
2024-07-13 18:58:16 +0200

    only merge sparql answers witch consist of one answer
    Signed-off-by: Nikolas Rauscher <nikolas.rauscher@gmail.com>

--- src/features/noise_reduction/generate_spaql/merge_anwsers.py
+++ src/features/noise_reduction/generate_spaql/merge_anwsers.py
@@ -19,28 +19,25 @@ def merge_answers(train_dataset_path, sparql_answers_path, output_file_path):
         train_data = json.load(train_file)
         print(f"Loaded {len(train_data)} items from the train dataset")
     
-
     with open(sparql_answers_path, 'r') as sparql_file:
         sparql_data = json.load(sparql_file)
         print(f"Loaded {len(sparql_data)} items from the SPARQL answers dataset")
     
-
-    sparql_dict = {item['id']: ', '.join(item['sparql_answer']) for item in sparql_data if item['sparql_answer']}
+    # Filter SPARQL answers to include only those with exactly one element in the list
+    sparql_dict = {item['id']: item['sparql_answer'][0] for item in sparql_data if len(item['sparql_answer']) == 1}
     print(f"Created dictionary with {len(sparql_dict)} SPARQL answers")
 
-
+    # Update the training data with the filtered SPARQL answers
     for item in train_data:
         if item['id'] in sparql_dict:
             item['answer'] = sparql_dict[item['id']]
     
-
     os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
     
     with open(output_file_path, 'w') as output_file:
         json.dump(train_data, output_file, indent=2)
     
     print(f'Merged data saved to {output_file_path} successfully!')
-
 
 train_dataset_path = 'src/features/noise_reduction/generate_spaql/datasets/train_dataset.json'
 sparql_answers_path = 'src/features/noise_reduction/generate_spaql/datasets/answers/filterd_awnsers/1000_qestions/no_numbers_filterd_awnsers.json'
