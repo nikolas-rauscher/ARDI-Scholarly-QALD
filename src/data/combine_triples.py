f209a208a6eb769199719f963016e87692122146
dendroenydris <yuxin.xue@campus.tu-berlin.de>
2024-07-31 18:36:21 +0200

    fix

--- src/data/combine_triples.py
+++ src/data/combine_triples.py
@@ -1,13 +1,14 @@
 import json
 import pandas as pd
 
+
 def merge_triples(all_triples):
     merged_triples = []
-    
+
     for entry in all_triples:
         triples = entry.get('triples', [])
         merged_triples.extend(triples)
-    
+
     return merged_triples
 
 
@@ -24,27 +25,31 @@ def create_merged_dataframe(df):
             "all_triples": merged_triples
         }
         records.append(record)
-    
+
     # Convert records to a DataFrame
     return pd.DataFrame(records)
 
 
-
 def merge_triples_and_create_dataset(alex_df, dblp_df):
     # Merge the 'all_triples' column, which is a list, from both dataframes
-    alex_df['all_triples'] = alex_df['all_triples'].apply(lambda x: x if isinstance(x, list) else [])
-    dblp_df['all_triples'] = dblp_df['all_triples'].apply(lambda x: x if isinstance(x, list) else [])
-    merged_triples = alex_df['all_triples'] + dblp_df['all_triples']
+    alex_df['all_triples'] = alex_df['all_triples'].apply(
+        lambda x: x if isinstance(x, list) else [])
+    dblp_df['all_triples'] = dblp_df['all_triples'].apply(
+        lambda x: x if isinstance(x, list) else [])
+
+    result_df = alex_df.merge(dblp_df, on='id', suffixes=('_alex', '_dblp'))
+    result_df['all_triples'] = result_df['all_triples_alex'] + \
+        result_df['all_triples_dblp']
 
     # Create a new dataframe with the required columns
     merged_df = pd.DataFrame({
         "id": alex_df["id"],
         "question": alex_df["question"],
         "author_dblp_uri": dblp_df["author_dblp_uri"],
-        "triples_number": merged_triples.apply(len),
-        "all_triples": merged_triples
+        "triples_number": result_df['all_triples'].apply(len),
+        "all_triples": result_df['all_triples']
     })
-    
+
     return merged_df
 
 
@@ -66,9 +71,13 @@ if __name__ == "__main__":
     merged_train_dblp_df = create_merged_dataframe(train_dblp_df)
 
     # Merge and create the test and train datasets
-    test_merged_df = merge_triples_and_create_dataset(merged_test_alex_df, merged_test_dblp_df)
-    train_merged_df = merge_triples_and_create_dataset(merged_train_alex_df, merged_train_dblp_df)
+    test_merged_df = merge_triples_and_create_dataset(
+        merged_test_alex_df, merged_test_dblp_df)
+    train_merged_df = merge_triples_and_create_dataset(
+        merged_train_alex_df, merged_train_dblp_df)
 
     # Save the merged datasets to JSON files
-    test_merged_df.to_json("./data/processed/test.json", orient='records', lines=True,indent=2)
-    train_merged_df.to_json("./data/processed/train.json", orient='records', lines=True,indent=2)
+    test_merged_df.to_json("./data/processed/test.json",
+                           orient='records', indent=2)
+    train_merged_df.to_json("./data/processed/train.json",
+                            orient='records', indent=2)
