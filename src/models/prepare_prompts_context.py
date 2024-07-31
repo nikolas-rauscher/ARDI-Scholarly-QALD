b15959b96118bde3fcad1efe4728bd25c490fd1a
dendroenydris <yuxin.xue@campus.tu-berlin.de>
2024-07-31 15:34:01 +0200

    add 4 setting's version

--- src/models/prepare_prompts_context.py
+++ src/models/prepare_prompts_context.py
@@ -1,12 +1,12 @@
+import os
+from tqdm import tqdm
+import configparser
+import json
+from models.verbalizer.generatePrompt import verbalise_triples
+from features.evidence_selection import evidence_triple_selection, triple2text, evidence_sentence_selection
 import sys
 sys.path.append('./src')
 sys.path.append('..')
-from features.evidence_selection import evidence_triple_selection, triple2text, evidence_sentence_selection
-from models.verbalizer.generatePrompt import verbalise_triples
-import json
-import configparser
-from tqdm import tqdm
-import os
 PACKAGE_PARENT = '.'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(
     os.path.join(os.getcwd(), os.path.expanduser(__file__))))
@@ -73,7 +73,7 @@ def prepare_data_only_ve(examples, prompt_template, output_file):
         json.dump(prepared_data, file, indent=4, ensure_ascii=False)
 
 
-def prepare_data_openAlex_dblp(examples, prompt_template, output_file):
+def prepare_data_4settings(examples, prompt_template, output_file, wiki=True):
     """
     Prepare the data by generating contexts for each example with dnlp and openalex
 
@@ -88,11 +88,25 @@ def prepare_data_openAlex_dblp(examples, prompt_template, output_file):
     prepared_data = []
     for example in tqdm(examples, desc="Preparing Data"):
 
-        tripples_number = len(example['all_triples'])
+        triples_number = len(example['all_triples'])
+
+        # wiki
+        wiki_context = ""
+        wiki_context_plain = ""
+        if (wiki):
+            for wiki_text in example['wiki_data']:
+                # sentences = ['. '.join(list(item.values())) for item in wiki_text]
+                wiki_context_plain += str(wiki_text)
+                if len(wiki_text) > 0:
+                    sentences = str(wiki_text).split('.')
+                    wiki_evidence = evidence_sentence_selection(
+                        example['question'], sentences, conserved_percentage=0.2, max_num=40
+                    )
+                    wiki_context += '. '.join(wiki_evidence)
 
         # Plain Triples
-        # context_plain = '. '.join([triple2text(triple)
-        #                           for triple in example['all_tripples']])
+        context_plain = '. '.join([triple2text(triple)
+                                  for triple in example['all_tripples']])
 
         # Evidence Matching
         triples_evidence = evidence_triple_selection(
@@ -101,7 +115,7 @@ def prepare_data_openAlex_dblp(examples, prompt_template, output_file):
             [triple2text(triple) for triple in triples_evidence])
 
         # Verbalizer
-        # context_verbalizer = verbalise_triples(example['all_tripples'])
+        context_verbalizer = verbalise_triples(example['all_tripples'])
 
         # Verbalizer + Evidence Matching
         context_evidence_verbalizer = verbalise_triples(triples_evidence)
@@ -109,16 +123,16 @@ def prepare_data_openAlex_dblp(examples, prompt_template, output_file):
         prepared_example = {
             "id": example["id"],
             "question": example["question"],
-            # "answer": example["answer"],
             "triples_number": triples_number,
             "contexts": {
-                # "all_tripples": example['all_tripples'],
-                # "plain": context_plain,
-                # "verbalizer_on_all_tripples": context_verbalizer,
-                "evidence_matching": context_evidence,
-                "verbalizer_plus_evidence_matching": context_evidence_verbalizer
+                "all_tripples": example['all_tripples'],
+                "plain": context_plain+wiki_context_plain,
+                "verbalizer_on_all_tripples": context_verbalizer+wiki_context_plain,
+                "evidence_matching": context_evidence+wiki_context,
+                "verbalizer_plus_evidence_matching": context_evidence_verbalizer+wiki_context
             }
         }
+        
         if ("answer" in example):
             prepared_example["answer"] = example["answer"]
         prepared_data.append(prepared_example)
