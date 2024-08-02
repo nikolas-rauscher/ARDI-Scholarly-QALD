09da69bc4a3e298751137569c4dea0fe20fcb229
Erik Rubinov <erik.rubinov@ottobock.com>
2024-08-02 17:05:17 +0200

    changes minor

--- run_question.py
+++ run_question.py
@@ -6,6 +6,19 @@ question = "What is the field of study of Tom Wilson?"
 author_dblp_uri = ["https://dblp.org/pid/w/TDWilson"]
 experiment_name = "test"
 
+RAW_Wikipedia_path = "data/external/wiki_data.txt"
+
+DBLP_endpoint_url ="https://dblp-april24.skynet.coypu.org/sparql" #SPARQL endpoint URL
+DBLP_name_outputdata_PREprocessed = "pre_processed" + experiment_name + ".json"
+DBLP_name_outputdata_POSTprocessed ="post_processed" + experiment_name + ".json"
+
+OpenAlex_endpoint_url ="https://semopenalex.org/sparql" # Alternative smaller KG: "https://semoa.skynet.coypu.org/sparql"
+OpenAlex_name_outputdata_PREprocessed = "pre_processed" + experiment_name + ".json"
+OpenAlex_name_outputdata_POSTprocessed = "post_processed" + experiment_name + ".json"
+
+Wikipedia_name_outputdata = "wiki_data_"+ experiment_name + ".json"
+
+Final_name_merged_data = "final_merged_data" + experiment_name+ ".json"
 
 
 def create_question_dict(question,author_dblp_uri,experiment_name):
