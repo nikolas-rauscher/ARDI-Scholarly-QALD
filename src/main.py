f386f33d1201b65d57424df06b4162907ff2a642
Erik Rubinov <erik.rubinov@ottobock.com>
2024-08-03 14:18:17 +0200

    adjust module imports

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import configparser
-from src.data.data_extraction.create_context import create_dataset
-from src.data.data_extraction.run_question import run_question
+from data.data_extraction.create_context import create_dataset
+from data.data_extraction.run_question import run_question
 
 class Config:
     def __init__(self, filename='config.ini'):
