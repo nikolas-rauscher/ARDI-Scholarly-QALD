ea6b8c73d1962ab0ec9bba6f7dfbabc2e22d5fbd
Erik Rubinov <erik.rubinov@ottobock.com>
2024-08-04 03:41:03 +0200

    add empty folders

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from data.data_extraction.run_question import run_question
 
 #Extract triples and add wikipedia context for file of questions (path in config.ini)
 experiment_name = "full_dataset"
-data_extraction(experiment_name)
+#data_extraction(experiment_name)
 
 ##################################################################################
 
@@ -13,6 +13,6 @@ data_extraction(experiment_name)
 question = "What is the field of study of Tom Wilson?"
 author_dblp_uri = ["https://dblp.org/pid/w/TDWilson"]
 
-#run_question(question, author_dblp_uri)
+run_question(question, author_dblp_uri)
 
 ##################################################################################
